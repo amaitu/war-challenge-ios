6bad9ed4cb632256a9141101cf7c07879898db09
amaitu <git@sgbarker.com>
2022-05-20 12:00:45 +0100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by amaitu on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
